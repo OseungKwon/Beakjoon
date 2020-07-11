451d8a649c3871025f46a0fdb4953362a21cbe94
OseungKwon <00osos@naver.com>
2020-07-11 18:25:08 +0900

    Create 실습1-평균점수.c

---
+++ 실습1-평균점수.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void) {
+	int score[5] = { 0, };
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", i);
+		scanf("%d", &score[i]);
+		if (score[i] < 40) {
+			score[i] = 40;
+		}
+		sum = sum + score[i];
+	}
+	puts("");
+	printf("%d", sum/5);
+}
