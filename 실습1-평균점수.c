e86a5653911dbd5505398a097aeef99c51366189
OseungKwon <00osos@naver.com>
2020-07-11 18:32:02 +0900

    Update 실습1-평균점수.c

--- 실습1-평균점수.c
+++ 실습1-평균점수.c
@@ -4,14 +4,11 @@
 int main(void) {
 	int score[5] = { 0, };
 	int sum = 0;
-	for (int i = 0; i < 5; i++) {
-		printf("%d ", i);
 		scanf("%d", &score[i]);
 		if (score[i] < 40) {
 			score[i] = 40;
 		}
 		sum = sum + score[i];
 	}
-	puts("");
 	printf("%d", sum/5);
 }
