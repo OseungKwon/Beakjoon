9bc216287715195ca55e9d87dae32b19c4058d3d
OseungKwon <00osos@naver.com>
2021-03-10 18:36:06 +0900

    Update 실습1-평균점수.c

--- 실습1-평균점수.c
+++ 실습1-평균점수.c
@@ -1,15 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS
+//bubble sort
 #include<stdio.h>
+BubbleSort(int arr[], int len) {
+	int temp;
+
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = 0; j < (len - i) - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
 
 int main(void) {
-	int score[5] = { 0, };
-	int sum = 0;
-	for (int i = 0; i < 5; i++) {
-		scanf("%d", &score[i]);
-		if (score[i] < 40) {
-			score[i] = 40;
-		}
-		sum = sum + score[i];
-	}
-	printf("%d", sum/5);
+	int len;
+	int arr[5] = { 2,4,1,5,3 };
+	len = sizeof(arr) / sizeof(int);
+
+	BubbleSort(arr, len);
+	for (int i = 0; i < len - 1; i++)
+		printf("%d ", arr[i]);
+	return 0;
 }
